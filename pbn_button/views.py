e6885823c337a562a1085c110e108fe00cb82d24
Lukas-Foniok <lukyfoniok@gmail.com>
2023-04-25 17:16:42 +0200

    i wuw u

--- pbn_button/views.py
+++ pbn_button/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
+from . models import Button
 
 # Create your views here.
 
@@ -28,4 +29,6 @@ def index(request):
         messages.success(request, 'You cannot access that without logging in!')
         return redirect('login_user')
     else:
-        return render(request, 'index.html', { 'is_logged_in': True })
+        if not Button.objects.first().exists():
+            Button.objects.create(state='False')
+        return render(request, 'index.html', { 'is_logged_in': True, "Button": Button.objects.first()})
